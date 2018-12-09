db530a16262a692371a90ce9657ba2ccc40566fb
Joshua Graciano <joshuagraciano@joshuas-macbook-pro.local>
2018-12-09 11:51:02 -0800

    Convert to MVC design pattern

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"] // these are just hard coded to be the text for the first 3 cells in the Table View. By changing the data type to var it becomes mutable.
+    var itemArray = [Item]() // we are changing this array to an array of item objects which hold info about the items added to the list in the Table View Controller
     
     let defaults = UserDefaults.standard // in order to use defaults, we have to create a new defaults object
     // User Defaults is an interface to the user's default database where you store key value pairs persistently across launches of your app. Here we are setting up a standard user default
@@ -19,9 +19,25 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }// here we are retrieving the data that was saved in an array in our User defaults
+        let newItem = Item()
+        newItem.title = "Find Mike" // changing the title property of the Item class
+        itemArray.append(newItem) // we are going to append the new element
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy eggos" // changing the title property of the Item class
+        itemArray.append(newItem2) // we are going to append the new element
+        
+        let newItem3 = Item()
+        newItem3.title = "Buy dog food" // changing the title property of the Item class
+        itemArray.append(newItem3) // we are going to append the new element
+        
+        
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] { // here we are trying to pull out our array from User Defaults as an array of items.
+            // here we are also checking to see if there is an array called TodoListArray to avoid the app crash if there isn't any array, then perform the following:
+            itemArray = items // we need to use the array saved in User defaults to retrieve the data. All we need to do to retrieve the data is we can go to viewDidLoad and we can set our itemArray to equal the array inside our User Default
+        } // here we are retrieving the data that was saved in an array in our User defaults
+        // here we are reactivating our defaults to try and retrieve our items out of our item Array
     }
     
     //MARK: - Tableview Datasource Methods
@@ -35,12 +51,32 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // we are going to create a cell in here that is going to be a resuable cell
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        // here we are saying go and find that prototype cell called 'ToDoItemCell' inside main.storyboard and generate a whole bunch of it that we can reuse
         // The identifier is the identifier which we gave to our prototype cell in main.storyboard
         // the indexPath is going to be the current index path that the Table View is looking to populate
         
+        let item = itemArray[indexPath.row] // this item is now the item that we're currently trying to set up for the cell
+        
         //Once we got our cell dequeued, the next step is to set the text label. This is the label that's in every single cell and we're going to set its text property and we're going to set it to equal the items in our item array
-        cell.textLabel?.text = itemArray[indexPath.row] // this is the current row of the current index that we are populating
+        cell.textLabel?.text = item.title // this is the current row of the current index that we are populating. We need to tap into the title property using the dot notation since this will return an Item object
+        
+        // here we are using a ternary operator
+        // Value = condition ? valueIfTrue : valueIfFalse
+        // We have this condition and we check to see if it is true or it is false. If its true, then we set this value to a particular value if its true and if it's not true then we set it to something else
+        
+        //        cell.accessoryType = item.done == true ? .checkmark :.none
+        // the above is the format for the ternary operator, however, you can go one step further and remove the '== true'
+        cell.accessoryType = item.done ? .checkmark : .none
+        // this expression is a smaller format that does the exact thing as the 5 lines below
+        // this cell now reads: set the cell's accessory type depending on whether the item.done is true. If it is true, then set it to checkmark. If it's not true, set it to .none
+        
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
         
         //final step is to return the cell
         return cell
@@ -54,14 +90,10 @@ class TodoListViewController: UITableViewController {
 //        print(itemArray[indexPath.row]) // this is going to print the element in the itemArray into the debug console when we select one of the cells which corresponds to the number of the cell which we selected.
 //         // this is going to print the element in the array that corresponds to the specific cell that is selected
         
-        // this method removes the checkmark when it's already been selected
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark // this is grabbing a reference to the cell that is at a particular index path
-            // the cell at this index path in our table is going to have an accessory type of .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        // this means that it sets the done property on the current item in the itemArray to the opposite of itemArray[indexPath.row].done (or what it is right now)
+        
+        tableView.reloadData() // it forces the Table View cell to call its data source methods again so that it reloads the data that's meant to be inside. The above checkpoint operation simply updates the data in the itemArray
         
         tableView.deselectRow(at: indexPath, animated: true) // now when you select a cell, it flashes gray briefly but then it goes back to being de-selected and goes back to be white which looks a lot nicer
         
@@ -81,28 +113,32 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once the user clicks the Add Item button on our UIAlert
             
-        self.itemArray.append(textField.text!) // we are going to force unwrap this because the text property of the textField is never going to equal nil even if its empty it's going to be set to an empty string
+        let newItem = Item()
+        newItem.title = textField.text! // here we are creating a new Item object and tapping into its title property and setting equal to the text that is entered
+            
+        self.itemArray.append(newItem) // here we are appending the new Item that is created above into our itemArray that contains Item objects.
             //we are appending whatever the user adds in the textfield to the array itemArray
             
-        self.defaults.set(self.itemArray, forKey: "TodoListArray") // here we can save the updated item array to User Defaults
+        self.defaults.set(self.itemArray, forKey: "TodoListArray") // here we can save the updated item array and set it into User Defaults
             // the key is going to identify this array inside our UserDefaults
             
         self.tableView.reloadData() // this reloads the rows and the sections of the TableView taking into account the new data we've added to our itemArray
             
-        print(textField.text) // to check if it the reference we created in the closure below works and is accessible globally. Prints everything that is added to the textfield after the Add Item button is pressed
+        print(textField.text!) // to check if it the reference we created in the closure below works and is accessible globally. Prints everything that is added to the textfield after the Add Item button is pressed
         
 
         } // press enter on the last parameter to create a completion handler or a closure. This is the completion block that gets called once that "Add Item" gets pressed
         
+        // in order to add a textfield to our alert (very similar to adding an action)
+        // hit enter on the placeholder parameter to insert closure
+        // we are going to call the textfield that we create 'alertTextField
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item" // this is the string that is going to show up in gray and is going to disappear when the user clicks on that textfield
             
             textField = alertTextField  // here we want to set the textField variable with a wider scope equal to the alertTextfield which has a narrower scope. We are extending the scope of this alertTextField to the addButtonPressed method.
             
             // the alertTextfield has the scope of this closure only (only available in here)
-        }// in order to add a textfield to our alert (very similar to adding an action)
-        // hit enter on the placeholder parameter to insert closure
-        // we are going to call the textfield that we create 'alertTextField
+        }
         
         alert.addAction(action) // now let's add our action to our alert
         
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Joshua Graciano on 12/5/18.
+//  Copyright © 2018 Joshua Graciano. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""// this is where we are going to save the titles of our To Do's
+    var done : Bool = false // this is where we are going to save whether the item was checked off or not. All items by default start off by being not done, so we set it to false.
+    
+}
